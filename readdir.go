4991172c83f6e8ae12312781aa4319b0d66e8582
Sheva Xu <shevaxu@outlook.com>
2017-11-13 18:16:33 +0800

    Add OS read-dir primitives

---
+++ readdir.go
@@ -0,0 +1,149 @@
+package watcher
+
+import (
+	"os"
+	"os/exec"
+	"strings"
+	"path/filepath"
+	//"fmt"
+)
+
+func ls(name string) ([]string, error) {
+	// TODO: cross platform
+	path, err := exec.LookPath("ls")
+	if err != nil {
+		return nil, err
+	}
+
+	// run and get the output
+	out, err := exec.Command(path, name).Output()
+	if err != nil {
+		return nil, err
+	}
+
+	ss := strings.Split(string(out), "\n")
+	if ss[len(ss)-1] == "" {
+		// trailing space
+		return ss[:len(ss)-1], nil
+	} else {
+		return ss, nil
+	}
+}
+
+func la(name string) ([]string, error) {
+	// TODO: cross platform
+	path, err := exec.LookPath("ls")
+	if err != nil {
+		return nil, err
+	}
+
+	// run and get the output
+	out, err := exec.Command(path, "-a", name).Output()
+	if err != nil {
+		return nil, err
+	}
+
+	ss := strings.Split(string(out), "\n")
+	if ss[len(ss)-1] == "" {
+		// trailing space
+		return ss[:len(ss)-1], nil
+	} else {
+		return ss, nil
+	}
+}
+
+func readDir(dirname string) ([]os.FileInfo, error) {
+	f, err := os.Open(dirname)
+	if err != nil {
+		return nil, err
+	}
+	f.Close()
+
+	names, err := la(dirname)
+	if err != nil {
+		return nil, err
+	}
+
+	list := make([]os.FileInfo, 0)
+	for _, name := range names {
+		if name == "." || name == ".." {
+			// ioutil.ReadDir ignores these
+			continue
+		}
+		info, err := os.Lstat(name)
+		if err != nil {
+			return list, err
+		}
+		list = append(list, info)
+	}
+
+	return list, nil
+}
+
+// from path.go
+// TODO: other way than monkey patching?
+
+// walk recursively descends path, calling w.
+func walk(path string, info os.FileInfo, walkFn filepath.WalkFunc) error {
+	err := walkFn(path, info, nil)
+	if err != nil {
+		if info.IsDir() && err == filepath.SkipDir {
+			return nil
+		}
+		return err
+	}
+
+	if !info.IsDir() {
+		return nil
+	}
+
+	//fmt.Println("Into dir", path)
+	// monkey-patching the read directory function
+	// names, err := readDirNames(path)
+	names, err := la(path)
+	if err != nil {
+		return walkFn(path, info, err)
+	}
+	//fmt.Println("Got names", len(names))
+
+	for _, name := range names {
+		if name == "." || name == ".." {
+			// ignore self and parent dir
+			continue
+		}
+		filename := filepath.Join(path, name)
+		fileInfo, err := os.Lstat(filename)
+		if err != nil {
+			if err := walkFn(filename, fileInfo, err); err != nil && err != filepath.SkipDir {
+				return err
+			}
+		} else {
+			err = walk(filename, fileInfo, walkFn)
+			if err != nil {
+				if !fileInfo.IsDir() || err != filepath.SkipDir {
+					return err
+				}
+			}
+		}
+	}
+	return nil
+}
+
+// Walk walks the file tree rooted at root, calling walkFn for each file or
+// directory in the tree, including root. All errors that arise visiting files
+// and directories are filtered by walkFn. The files are walked in lexical
+// order, which makes the output deterministic but means that for very
+// large directories Walk can be inefficient.
+// Walk does not follow symbolic links.
+func Walk(root string, walkFn filepath.WalkFunc) error {
+	info, err := os.Lstat(root)
+	if err != nil {
+		err = walkFn(root, nil, err)
+	} else {
+		err = walk(root, info, walkFn)
+	}
+	if err == filepath.SkipDir {
+		return nil
+	}
+	return err
+}
---
+++ readdir_test.go
@@ -0,0 +1,38 @@
+package watcher
+
+import (
+	"testing"
+	"io/ioutil"
+
+	"github.com/ShevaXu/web-utils/assert"
+)
+
+const currentDir = "."
+
+func TestLs(t *testing.T) {
+	names, err := ls(currentDir)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(names)
+}
+
+func TestLa(t *testing.T) {
+	names, err := la(currentDir)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(names)
+}
+
+func TestReadDir(t *testing.T) {
+	a := assert.NewAssert(t)
+
+	list, err := readDir(currentDir)
+	a.NoError(err, "readDir should succeed")
+
+	list2, err := ioutil.ReadDir(currentDir)
+	a.NoError(err, "ioutil.ReadDir should succeed")
+
+	a.Equal(list, list2, "Returns the same")
+}
